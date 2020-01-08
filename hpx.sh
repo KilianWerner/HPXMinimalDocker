d3c6f70ca1d95962d4940259669a8b381375e95c
Kilian Werner <kwerner@rhrk.uni-kl.de>
2020-01-08 13:55:18 +0100

    parcelports

--- hpx.sh
+++ hpx.sh
@@ -2,6 +2,6 @@ mkdir hpx
 cd hpx
 git clone https://github.com/STEllAR-GROUP/hpx.git --branch 1.3.0 --single-branch
 mkdir build-hpx && cd build-hpx
-cmake ../hpx
+cmake ../hpx -DHPX_WITH_PARCELPORT_MPI=ON -DHPX_WITH_PARCELPORT_TCP=OFF
 make -j 8
 make install
