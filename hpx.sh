fdc4272a6904fcae7e7abecd3e5898c31fd7d743
Kilian Werner <kwerner@rhrk.uni-kl.de>
2020-01-08 16:59:50 +0100

    latest stable hpx version, consistent use of cxx 17

--- hpx.sh
+++ hpx.sh
@@ -1,6 +1,6 @@
 mkdir hpx
 cd hpx
-git clone https://github.com/STEllAR-GROUP/hpx.git --branch 1.3.0 --single-branch
+git clone https://github.com/STEllAR-GROUP/hpx.git --branch stable --single-branch
 mkdir build-hpx && cd build-hpx
 cmake ../hpx -DHPX_WITH_PARCELPORT_MPI=ON -DHPX_WITH_PARCELPORT_TCP=OFF
 make -j 8
